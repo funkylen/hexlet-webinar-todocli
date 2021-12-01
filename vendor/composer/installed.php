b3ca27ca3a436da804a51318c98dc89e3d1f816f
funkylen <funkylen@gmail.com>
2021-12-01 20:22:32 +0300

    Make app structure

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9bb73a458748b32131926a5d6ef6f9392c71eb39',
+        'name' => 'funkylen/todocli',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'funkylen/todocli' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9bb73a458748b32131926a5d6ef6f9392c71eb39',
+            'dev_requirement' => false,
+        ),
+    ),
+);
