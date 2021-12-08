e0c72880db8fab3dfb323de3c02571c3891a0d1b
funkylen <funkylen@gmail.com>
2021-12-08 22:47:08 +0300

    Add check file function and add gitignore for storage

--- src/Storages/FilesystemStorage.php
+++ src/Storages/FilesystemStorage.php
@@ -14,9 +14,18 @@ function getTodoList(): array
 
 function getTodoListFromFile(string $filePath): array
 {
+    checkFileAndCreateIfNotExists($filePath);
+
     $content = file_get_contents($filePath);
 
     return json_decode($content);
+}
+
+function checkFileAndCreateIfNotExists(string $filePath): void
+{
+    if (!file_exists($filePath)) {
+        touch($filePath);
+    }
 }
 
 function saveTodoList(array $todoList): void
@@ -26,6 +35,8 @@ function saveTodoList(array $todoList): void
 
 function saveTodoListInFile(array $todoList, string $filePath)
 {
+    checkFileAndCreateIfNotExists($filePath);
+
     $todoListJsonString = json_encode($todoList, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
     file_put_contents($filePath, $todoListJsonString);
